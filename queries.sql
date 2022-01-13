879a3971142484de19ef8f801afc70d84b8b79e7
Tolihama <73126940+Tolihama@users.noreply.github.com>
2022-01-13 14:19:53 +0100

    queries point 3 fix

--- queries.sql
+++ queries.sql
@@ -12,7 +12,8 @@ WHERE `cfu` > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT * 
 FROM `students` 
-WHERE `date_of_birth` > DATE_SUB(CURRENT_DATE(), INTERVAL 30 YEAR);
+WHERE `date_of_birth` < DATE_SUB(CURRENT_DATE(), INTERVAL 30 YEAR)  
+ORDER BY `date_of_birth` DESC;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * 
